be5ac3a3379e399cdfe8afd2cc5f62e60cbab77a
Jens Nordgaard <jenordgaard@gmail.com>
2016-04-17 19:31:59 -0700

    This is the completed oop exercise from prework

---
+++ oop_todo.rb
@@ -0,0 +1,41 @@
+require_relative "list"
+require_relative "task"
+
+# Create list
+list = List.new
+
+# Create tasks and add them to the list
+list.add_task("Feed the cat", 2)
+list.add_task("Take out trash", 6)
+list.add_task("Mow the lawn", 4)
+
+# Print out the second task in the list
+puts "Second task:"
+puts list.tasks[1].name
+puts "---------"
+
+# Get an array containing the names of all incomplete tasks from the list
+puts "Incomplete Tasks:"
+puts list.incomplete_tasks
+puts "--------"
+
+
+# Mark the first task from the list as complete
+list.tasks[0].complete!
+puts list.tasks[0].complete
+list.tasks[0].toggle_complete!
+puts list.tasks[0].complete
+list.tasks[0].toggle_complete!
+puts list.tasks[0].complete
+
+
+# Print out the incomplete tasks again
+puts "Incomplete Tasks:"
+puts list.incomplete_tasks
+puts list.number_of_incomplete_tasks
+list.delete_complete_tasks
+p list.tasks
+list.tasks[2].priority = 8
+puts list.tasks.last.priority
+p list
+p list.rank_priorities
---
+++ task.rb
@@ -0,0 +1,22 @@
+class Task
+  attr_accessor :name, :complete, :priority
+
+  def initialize(name, priority)
+    @name = name
+    @priority = priority
+    @complete = false
+  end
+
+  def complete!
+    @complete = true
+  end
+
+  def toggle_complete!
+    @complete = !@complete
+  end
+
+  def set_priority(priority)
+    @priority = priority
+  end
+  
+end
